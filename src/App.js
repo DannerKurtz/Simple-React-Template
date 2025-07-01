9482929636dad1fd49132c03776e29da4c2d5a19
Danner Kurtz <kurtz.danner@gmail.com>
2025-07-01 20:11:53 -0300

    entendendo o que é o jsx

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
 const App = () => {
+  const array = [1, 2, 3, 4, 5];
+  const arrayDeFrutas = [
+    { fruta: 'laranja', cor: 'laranja' },
+    { fruta: 'uva', cor: 'roxa' },
+    { fruta: 'maça', cor: 'vermelha' },
+  ];
   return (
     <div>
       <h1>Configurando React</h1>
+      {array.map((item) => {
+        return (
+          <>
+            <li>
+              {item} * 5 = {item * 5}
+            </li>
+          </>
+        );
+      })}
+      <br></br>
+      <table>
+        <tr>
+          <th>Fruta</th>
+          <th>Cor</th>
+        </tr>
+        {arrayDeFrutas.map((item) => {
+          return (
+            <>
+              <tr>
+                <td>{item.fruta}</td>
+                <td>{item.cor}</td>
+              </tr>
+            </>
+          );
+        })}
+      </table>
     </div>
   );
 };
